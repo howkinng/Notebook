c1e42e3f80f38a898a534d406f452cd49584ee4f
79819 <79819@HOWKINNG>
2023-02-24 14:37:14 +0300

    Добавьте файлы проекта.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections.Generic;
+using System.Windows;
+
+namespace Notebook
+{
+    public partial class MainWindow : Window
+    {
+        private List<string> notes;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            notes = new List<string>();
+            lstNotes.ItemsSource = notes;
+        }
+
+        private void btnAdd_Click(object sender, RoutedEventArgs e)
+        {
+            string note = "New note";
+            notes.Add(note);
+            lstNotes.SelectedItem = note;
+            lstNotes.ScrollIntoView(note);
+            txtNote.Text = note;
+            txtNote.Visibility = Visibility.Visible;
+            txtNote.Focus();
+        }
+
+        private void btnEdit_Click(object sender, RoutedEventArgs e)
+        {
+            if (lstNotes.SelectedIndex >= 0)
+            {
+                txtNote.Text = lstNotes.SelectedItem.ToString();
+                txtNote.Visibility = Visibility.Visible;
+                txtNote.Focus();
+            }
+        }
+
+        private void btnDelete_Click(object sender, RoutedEventArgs e)
+        {
+            if (lstNotes.SelectedIndex >= 0)
+            {
+                notes.Remove(lstNotes.SelectedItem.ToString());
+            }
+        }
+
+        private void lstNotes_SelectionChanged(object sender, System.Windows.Controls.SelectionChangedEventArgs e)
+        {
+            if (lstNotes.SelectedIndex >= 0)
+            {
+                txtNote.Text = lstNotes.SelectedItem.ToString();
+                txtNote.Visibility = Visibility.Visible;
+            }
+            else
+            {
+                txtNote.Visibility = Visibility.Collapsed;
+            }
+        }
+
+        private void txtNote_LostFocus(object sender, RoutedEventArgs e)
+        {
+            if (lstNotes.SelectedIndex >= 0)
+            {
+                notes[lstNotes.SelectedIndex] = txtNote.Text;
+                lstNotes.SelectedItem = txtNote.Text;
+            }
+            txtNote.Visibility = Visibility.Collapsed;
+        }
+    }
+}
